4a341a4d8bc8beb56c3b6ab92545101c813735ed
virajpant <vpant9211@gmail.com>
2024-11-29 12:24:00 +0530

    done

--- app/teacher/dashboard/layout.js
+++ app/teacher/dashboard/layout.js
@@ -4,6 +4,8 @@ import { ToastContainer } from "react-toastify";
 import { UserProvider } from "@/providers/userProvider";
 import LayoutFile from "@/components/teacher/LayoutFile";
 import 'react-toastify/dist/ReactToastify.css';
+import { TestSeriesProvider } from "@/providers/testSeriesProvider";
+import { TestProvider } from "@/providers/testProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,8 +20,11 @@ export default function RootLayout({ children }) {
     <div lang="en">
       <ToastContainer />
       <UserProvider>
-        {" "}
+        <TestProvider>
+        <TestSeriesProvider>
         <LayoutFile>{children}</LayoutFile>
+        </TestSeriesProvider>
+        </TestProvider>
       </UserProvider>
     </div>
   );
